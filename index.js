73e68103036ff464175ee8ed3d74e872eae9a8a6
John Soto <jsoto1@ufl.edu>
2022-05-27 02:38:56 -0400

    added questions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+var questionsArr = [
+    {
+        question: 'The mascot of University of Florida is a dinosaur',
+        answer: false,
+    },
+    {
+        question: 'The mascot of Miami Dade College is a shark',
+        answer: true,
+    },
+    {
+        question: 'The mascot of University of Miami is a flamingo',
+        answer: false,
+    },
+    {
+        question: 'The mascot of Florida International University is a panther ',
+        answer: true,
+    },
+    {
+        question: 'The mascot of Barry University is a parrot',
+        answer: true,
+    },
+];
+
+function runQuiz() {
+    console.log(questionsArr);
+}
