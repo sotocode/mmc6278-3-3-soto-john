bf35302e17ac1a5136d7f5507624dfca9733cf10
John Soto <jsoto1@ufl.edu>
2022-05-28 20:56:39 -0400

    updated question string

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var questionsArr = [
     {
-        question: 'The mascot of University of Florida is a dinosaur',
+        question: 'The mascot of University of Florida is a dinosaur.',
         answer: false,
     },
     {
-        question: 'The mascot of Miami Dade College is a shark',
+        question: 'The mascot of Miami Dade College is a shark.',
         answer: true,
     },
     {
-        question: 'The mascot of University of Miami is a flamingo',
+        question: 'The mascot of University of Miami is a flamingo.',
         answer: false,
     },
     {
-        question: 'The mascot of Florida International University is a panther ',
+        question: 'The mascot of Florida International University is a panther.',
         answer: true,
     },
     {
-        question: 'The mascot of Barry University is a parrot',
+        question: 'The mascot of Barry University is a parrot.',
         answer: true,
     },
 ]
